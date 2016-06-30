da133c957d6f770641693acc331dde5c628cb6e2
Karl Stoney <kstoney@thoughtworks.com>
2016-06-30 14:41:30 +0100

    Added server route for adding stock pics

--- test/server.js
+++ test/server.js
@@ -1,9 +1,11 @@
 'use strict';
-let Server = require('sn.core').Server;
+let App = require('../lib/app');
 let restify = require('restify');
+let should = require('should');
+let deride = require('deride');
 
 describe('Server', () => {
-  let client, server;
+  let client, app, mockSymbolManager;
 
   before(done => {
     let targetPort = 9001;
@@ -11,11 +13,23 @@ describe('Server', () => {
       url: 'http://127.0.0.1:' + targetPort,
       version: require('../package.json').version
     }); 
-    server = new Server(targetPort);
-    server.start(done);
+    mockSymbolManager = deride.stub(['add']);
+    mockSymbolManager.setup.add.toCallbackWith(null);
+    app = new App(targetPort, mockSymbolManager);
+    app.start(done);
   });
 
   after(() => {
-    server.stop();
+    app.stop();
   });
+
+  it.only('should accept PUT requests for symbols', (done) => {
+    client.put('/symbol/LON/VM', null, (err, req, res) => {
+      should.ifError(err);
+      should(res.statusCode).eql(200);
+      mockSymbolManager.expect.add.called.once();
+      done();
+    });
+  });
+
 });
