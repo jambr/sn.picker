b351e9fdecc2fa4fcab5c0fb6bb8640fdfc6444c
Karl Stoney <kstoney@thoughtworks.com>
2016-06-30 14:45:39 +0100

    Added a delete symbol route

--- test/server.js
+++ test/server.js
@@ -13,8 +13,9 @@ describe('Server', () => {
       url: 'http://127.0.0.1:' + targetPort,
       version: require('../package.json').version
     }); 
-    mockSymbolManager = deride.stub(['add']);
+    mockSymbolManager = deride.stub(['add', 'remove']);
     mockSymbolManager.setup.add.toCallbackWith(null);
+    mockSymbolManager.setup.remove.toCallbackWith(null);
     app = new App(targetPort, mockSymbolManager);
     app.start(done);
   });
@@ -23,10 +24,19 @@ describe('Server', () => {
     app.stop();
   });
 
-  it.only('should accept PUT requests for symbols', (done) => {
+  it('should accept PUT requests for symbols', (done) => {
     client.put('/symbol/LON/VM', null, (err, req, res) => {
       should.ifError(err);
-      should(res.statusCode).eql(200);
+      should(res.statusCode).eql(201);
+      mockSymbolManager.expect.add.called.once();
+      done();
+    });
+  });
+
+  it('should accept DELETE requests for symbols', (done) => {
+    client.del('/symbol/LON/VM', (err, req, res) => {
+      should.ifError(err);
+      should(res.statusCode).eql(204);
       mockSymbolManager.expect.add.called.once();
       done();
     });
