5da644d32284c47b3d2c2ed5cf834105a270df13
Karl Stoney <kstoney@thoughtworks.com>
2016-06-30 16:47:00 +0100

    Idempotent API requests

--- test/server.js
+++ test/server.js
@@ -13,11 +13,19 @@ describe('Server', () => {
       url: 'http://127.0.0.1:' + targetPort,
       version: require('../package.json').version
     }); 
-    mockSymbolManager = deride.stub(['add', 'remove']);
-    mockSymbolManager.setup.add.toCallbackWith(null);
-    mockSymbolManager.setup.remove.toCallbackWith(null);
+    mockSymbolManager = deride.stub(['get', 'add', 'remove']);
     app = new App(targetPort, mockSymbolManager);
     app.start(done);
+  });
+
+  beforeEach(() => {
+    mockSymbolManager.setup.add.toCallbackWith(null);
+    mockSymbolManager.setup.remove.toCallbackWith(null);
+    mockSymbolManager.setup.get.toCallbackWith(null);
+
+    mockSymbolManager.expect.add.called.reset();
+    mockSymbolManager.expect.remove.called.reset();
+    mockSymbolManager.expect.get.called.reset();
   });
 
   after(() => {
@@ -33,11 +41,34 @@ describe('Server', () => {
     });
   });
 
+  it('adding the same symbol twice should result in a not modified', (done) => {
+    client.put('/symbol/LON/VM', null, () => {
+      mockSymbolManager.setup.get.toCallbackWith(null, { some: 'model' });
+      client.put('/symbol/LON/VM', null, (err, req, res) => {
+        should.ifError(err);
+        should(res.statusCode).eql(304);
+        mockSymbolManager.expect.add.called.once();
+        done();
+      });
+    });
+  });
+
   it('should accept DELETE requests for symbols', (done) => {
+    mockSymbolManager.setup.get.toCallbackWith(null, { some: 'model' });
     client.del('/symbol/LON/VM', (err, req, res) => {
       should.ifError(err);
       should(res.statusCode).eql(204);
-      mockSymbolManager.expect.add.called.once();
+      mockSymbolManager.expect.remove.called.once();
+      done();
+    });
+  });
+
+  it('deleting a symbol that doesnt exist should return bad request', (done) => {
+    mockSymbolManager.setup.get.toCallbackWith(null, undefined);
+    client.del('/symbol/LON/VM', (err, req, res) => {
+      should(err).not.eql(null, 'BadRequest error was not returned');
+      should(res.statusCode).eql(400);
+      mockSymbolManager.expect.remove.called.never();
       done();
     });
   });
